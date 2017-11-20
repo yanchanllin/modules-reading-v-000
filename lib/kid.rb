dd882dd05957e5e02b8fcc7b49f0ec1ac8bab0a1
YanC Lin <yonalin1@gmail.com>
2017-11-20 21:46:18 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -5,9 +5,9 @@ class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 
-  # attr_accessor :name
-  #
-  # def initialize(name)
-  #   @name = name
-  # end
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
